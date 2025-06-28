d9d4f7c3a73617a675f282d82ea19421ad3e5bda
tanishqpoornima <2020cscloudtanishq8459@poornima.edu.in>
2025-06-29 01:58:41 +0530

    Website email change

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -76,7 +76,7 @@ const Contact = () => {
     console.log("Form submitted:", formData);
   
     try {
-      const response = await fetch("http://localhost:5000/api/send-email", {
+      const response = await fetch("https://www.rtv-solutions-pvt.com/api/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
