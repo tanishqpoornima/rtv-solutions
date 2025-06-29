da85827b6c55a6b2c9d2d29cd3beff9e97818945
Tanishq Jain <87896784+tanishqpoornima@users.noreply.github.com>
2025-06-29 20:31:25 +0530

    Update index.js

--- api/send-email/index.js
+++ api/send-email/index.js
@@ -1,23 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-import sendEmail from './send-email.js';
-import dotenv from 'dotenv';
+const sendEmail = require('./send-email');
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-  origin: ['https://rtv-solutions-pvt.com', 'https://www.rtv-solutions-pvt.com'], // allow both
-  methods: ['POST'],
-  allowedHeaders: ['Content-Type']
-}));
-
-app.use(express.json());
-
-app.post('/api/send-email', sendEmail);
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+module.exports = async function (context, req) {
+  context.res = await sendEmail(req);
+};
